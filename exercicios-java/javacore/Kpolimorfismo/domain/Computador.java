30be42e5c63d3efbe413ff38582a161852bf0359
João Rocha <rochajoaao@gmail.com>
2026-03-13 19:21:42 -0300

    Adicionando polimorfismo

--- exercicios-java/javacore/Kpolimorfismo/domain/Computador.java
+++ exercicios-java/javacore/Kpolimorfismo/domain/Computador.java
@@ -1,7 +1,15 @@
 package javacore.Kpolimorfismo.domain;
 
 public class Computador extends Produto {
+    public static final double IMPOSTO_POR_CENTO = 0.21;
+
     public Computador(String nome, double valor) {
         super(nome, valor);
+    }
+
+    @Override
+    public double calcularImposto() {
+        System.out.println("Calculando imposto do computador");
+        return this.valor * IMPOSTO_POR_CENTO;
     }
 }
